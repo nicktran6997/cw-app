eaca029da4e235f129d79d263e2f23234085f997
Robert Elwell <robert.elwell@gmail.com>
2017-06-18 11:36:54 -0400

    current progress refactoring the search component

--- app/containers/Search/actions.js
+++ app/containers/Search/actions.js
@@ -5,15 +5,26 @@
  */
 import client from '../../utils/client';
 import {
-  DEFAULT_ACTION,
+  SEARCH_ACTION,
+  PAGE_CHANGE_ACTION,
+  AGG_SELECTED_ACTION,
+  AGG_REMOVED_ACTION,
+  TOGGLE_SORT_ACTION,
+  QUERY_CHANGE_ACTION,
 } from './constants';
 
-export const defaultAction = (dispatch) =>
-  (params) =>
-    client.post(`/studies/search/${params.query}/json`, params)
+export const searchAction = (dispatch) =>
+  (params) => {
+    let url;
+    if (params.query) {
+      url = `/studies/search/${params.query}/json`;
+    } else {
+      url = '/studies/json';
+    }
+    return client.post(url, params)
       .then((data) =>
         dispatch({
-          type: DEFAULT_ACTION,
+          type: SEARCH_ACTION,
           data: Object.assign(data.data, {
             query: params.query,
             page: params.page,
@@ -21,3 +32,68 @@ export const defaultAction = (dispatch) =>
             sorts: params.sorts,
           }),
         }));
+  };
+
+export const pageChangeAction = (dispatch) =>
+  (args) =>
+    Promise.resolve(dispatch({
+      type: PAGE_CHANGE_ACTION,
+      data: args,
+    }));
+
+export const selectAggAction = (dispatch) =>
+  (field, key) =>
+    Promise.resolve(dispatch({
+      type: AGG_SELECTED_ACTION,
+      data: { [field]: { [key]: 1 } },
+    }));
+
+export const removeAggAction = (dispatch) =>
+  (field, key) =>
+    Promise.resolve(dispatch({
+      type: AGG_REMOVED_ACTION,
+      data: [field, key],
+    }));
+
+export const toggleSortAction = (dispatch) =>
+  (field) =>
+    Promise.resolve(dispatch({
+      type: TOGGLE_SORT_ACTION,
+      data: field,
+    }));
+
+export const queryChangeAction = (dispatch) =>
+  (query) => Promise.resolve(dispatch({
+    type: QUERY_CHANGE_ACTION,
+    data: query,
+  }));
+
+export const getSearchParams = (props, query) => Object.assign({
+  query: query || getQuery(props),
+  start: (props.Search.page) * props.pageLength,
+  length: props.pageLength,
+  page: props.Search.page,
+  sorts: props.Search.sorts,
+}, getAggsObject(props));
+
+export const getQuery = (props) => {
+  if (props.Search && props.Search.query) {
+    return props.Search.query;
+  }
+  if (props.params && props.params.query) {
+    return props.params.query;
+  }
+  if (!props.location.pathname.match(/\/search\/$/) &&
+      props.Auth && props.Auth.user
+      && props.Auth.user.default_query_string) {
+    return props.Auth.user.default_query_string;
+  }
+  return '';
+};
+
+export const getAggsObject = (props) => {
+  if (props.Search.aggsSent) {
+    return { agg_filters: props.Search.aggsSent };
+  }
+  return {};
+};
--- app/containers/Search/reducer.js
+++ app/containers/Search/reducer.js
@@ -6,14 +6,25 @@
 
 import { fromJS } from 'immutable';
 import {
-  DEFAULT_ACTION,
+  SEARCH_ACTION,
+  PAGE_CHANGE_ACTION,
+  AGG_SELECTED_ACTION,
+  AGG_REMOVED_ACTION,
+  TOGGLE_SORT_ACTION,
+  QUERY_CHANGE_ACTION,
 } from './constants';
 
-const initialState = fromJS({});
+const initialState = fromJS({
+  total: 0,
+  page: 0,
+  aggsSent: {},
+  sorts: {},
+  aggs: {},
+});
 
 function searchReducer(state = initialState, action) {
   switch (action.type) {
-    case DEFAULT_ACTION:
+    case SEARCH_ACTION:
       return state
         .set('query', action.data.query)
         .set('aggsSent', action.data.aggsSent)
@@ -22,6 +33,31 @@ function searchReducer(state = initialState, action) {
         .set('totalFiltered', action.data.recordsFiltered)
         .set('rows', action.data.data)
         .set('aggs', action.data.aggs);
+    case PAGE_CHANGE_ACTION:
+      return state.set('page', action.data.selected);
+    case AGG_SELECTED_ACTION:
+      return state.set('aggsSent',
+        Object.assign({}, action.data, state.get('aggsSent')))
+        .set('page', 0);
+    case AGG_REMOVED_ACTION:
+      return state.set('aggsSent',
+        () => {
+          const aggs = state.get('aggsSent');
+          delete aggs[action.data[0]][action.data[1]];
+          return aggs;
+        })
+        .set('page', 0);
+    case TOGGLE_SORT_ACTION:
+      switch (state.getIn(['sorts', action.data])) {
+        case 'asc':
+          return state.setIn(['sorts', action.data], 'desc');
+        case 'desc':
+          return state.deleteIn(['sorts', action.data]);
+        default:
+          return state.setIn(['sorts', action.data], 'asc');
+      }
+    case QUERY_CHANGE_ACTION:
+      return state.set('query', action.data).set('page', 0);
     default:
       return state;
   }
