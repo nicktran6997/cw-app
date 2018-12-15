5c384abd7bfe01be8aa95cc98d8cdc879b0ebfe8
Eric Harding <eric.harding@bluetradesys.com>
2018-12-15 17:13:39 -0500

    WIP: Add video support to TextEditor

--- app/components/TextEditor/Index.tsx
+++ app/components/TextEditor/Index.tsx
@@ -1,15 +1,49 @@
 import * as React from 'react';
-import RichTextEditor from 'react-rte';
+import RichTextEditor, {ButtonGroup, Button, PopoverIconButton} from 'react-rte';
 import FontAwesome from 'react-fontawesome';
+import * as Immutable from 'immutable';
+
+// serialization options:
+// 1 - Store someting custom in markdown
+// 2 - store serialized editor state
+// 3 - store images and render them as videos
 
 // Wrap RichTextEditor for video support
 export default class TextEditor extends React.PureComponent<any> {
+  state = {
+    // value : RichTextEditor.editov
+    showVideoInput : false 
+  }
+
+  toggleVideoInput = () => {
+    this.setState({ showVideoInput: !this.state.showVideoInput})
+  }
+  insertVideo = (userInput, editorState) => {
+    console.log(userInput);
+  }
 
   render() {
+    console.log(`rte: ${PopoverIconButton}`)
     // Add 'react-rte' button to insert video
     // render by passing blockRenderMap though
     // extra props pass though to draft-js Editor
-    return <RichTextEditor {...this.props} />
+    return <RichTextEditor 
+      {...this.props}
+      customControls={[
+        (setValue,getValue,editorState) => {
+          return <ButtonGroup key={"cw-buttons"}>
+            <Button 
+              label="Video"
+              iconName="image"
+              focusOnClick={false}
+              showPopover={this.state.showVideoInput}
+              onTogglePopover={this.toggleVideoInput}
+              onSubmit={s => this.insertVideo(s, editorState)}
+            />
+          </ButtonGroup>
+        }
+      ]}
+     />
   }
 }
 
